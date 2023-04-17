b64e88cabd11f7f490ba2f038eaac29b3c8756d6
UBF21 <flashgames104@gmail.com>
2023-04-17 15:33:08 -0500

    Agregando una ruta de login en el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePagePetspitComponent } from './petspit/home-page-petspit/home-page-petspit.component';
 import { ContactPagePetspitComponent } from './petspit/contact-page-petspit/contact-page-petspit.component';
 import { ProductsPagePetspitComponent } from './petspit/products-page-petspit/products-page-petspit.component';
+import { LoginPageComponent } from './dashboard/auth/login-page/login-page.component';
 
 const routes: Routes = [
   {path:'',component:HomePagePetspitComponent,pathMatch:'full'},
   {path:'home' ,component:HomePagePetspitComponent, pathMatch: 'full'},
   {path:'contact' ,component:ContactPagePetspitComponent, pathMatch: 'full'},
   {path:'products' ,component:ProductsPagePetspitComponent,pathMatch: 'full'},
+  {path:'login' ,component:LoginPageComponent,pathMatch: 'full'},
   {path:'**' ,redirectTo:'/home',pathMatch: 'full'}
 ];
 
