926473a80cae782bd920f8f03193666387db230f
UBF21 <flashgames104@gmail.com>
2023-04-17 04:41:08 -0500

    Importando y exportando los modulos o componentes necesarios en sharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarPetspitComponent } from './nav-bar-petspit/nav-bar-petspit.component';
+import { NavsPetspitComponent } from './navs-petspit/navs-petspit.component';
+import { FooterPetspitComponent } from './footer-petspit/footer-petspit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavBarPetspitComponent,
+    NavsPetspitComponent,
+    FooterPetspitComponent
+  ],
   imports: [
     CommonModule
   ],
   exports: [
-
+    NavBarPetspitComponent,
+    NavsPetspitComponent,
+    FooterPetspitComponent
   ],
   providers: [
 
