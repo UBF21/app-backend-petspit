e0abcf7d72f046af75f63b8ae2402388bcdd5561
UBF21 <flashgames104@gmail.com>
2023-04-17 04:42:13 -0500

    Importando y exportando los modulos o componentes necesarios en PetspitModule

--- src/app/petspit/petspit.module.ts
+++ src/app/petspit/petspit.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePagePetspitComponent } from './home-page-petspit/home-page-petspit.component';
+import { ContactPagePetspitComponent } from './contact-page-petspit/contact-page-petspit.component';
+import { ProductsPagePetspitComponent } from './products-page-petspit/products-page-petspit.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePagePetspitComponent,
+    ContactPagePetspitComponent,
+    ProductsPagePetspitComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
-
+    HomePagePetspitComponent,
+    ContactPagePetspitComponent,
+    ProductsPagePetspitComponent
   ],
   providers: [
 
