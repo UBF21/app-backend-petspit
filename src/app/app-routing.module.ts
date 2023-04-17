978a8943444749b6c4f81c51689100088734e09a
UBF21 <flashgames104@gmail.com>
2023-04-17 14:54:17 -0500

    Definiendo las rutas de las paginas ya existentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePagePetspitComponent } from './petspit/home-page-petspit/home-page-petspit.component';
+import { ContactPagePetspitComponent } from './petspit/contact-page-petspit/contact-page-petspit.component';
+import { ProductsPagePetspitComponent } from './petspit/products-page-petspit/products-page-petspit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomePagePetspitComponent,pathMatch:'full'},
+  {path:'home' ,component:HomePagePetspitComponent, pathMatch: 'full'},
+  {path:'contact' ,component:ContactPagePetspitComponent, pathMatch: 'full'},
+  {path:'products' ,component:ProductsPagePetspitComponent,pathMatch: 'full'},
+  {path:'**' ,redirectTo:'/home',pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
