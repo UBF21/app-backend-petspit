3229cc1143ca12a1e02617002162ce4287dd6dae
UBF21 <flashgames104@gmail.com>
2023-04-23 02:11:50 -0500

    Creando la ruta par about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomePagePetspitComponent } from './petspit/home-page-petspit/home-page-
 import { ContactPagePetspitComponent } from './petspit/contact-page-petspit/contact-page-petspit.component';
 import { ProductsPagePetspitComponent } from './petspit/products-page-petspit/products-page-petspit.component';
 import { LoginPageComponent } from './dashboard/auth/login-page/login-page.component';
+import { AboutPagePetspitComponent } from './petspit/about-page-petspit/about-page-petspit.component';
 
 const routes: Routes = [
   {path:'',component:HomePagePetspitComponent,pathMatch:'full'},
   {path:'home' ,component:HomePagePetspitComponent, pathMatch: 'full'},
   {path:'contact' ,component:ContactPagePetspitComponent, pathMatch: 'full'},
   {path:'products' ,component:ProductsPagePetspitComponent,pathMatch: 'full'},
+  {path:'about' ,component:AboutPagePetspitComponent,pathMatch: 'full'},
   {path:'login' ,component:LoginPageComponent,pathMatch: 'full'},
   {path:'**' ,redirectTo:'/home',pathMatch: 'full'}
 ];
