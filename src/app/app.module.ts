05447946f3c1714a281b9dfa4684a1389c4b2d6b
UBF21 <flashgames104@gmail.com>
2023-04-24 14:45:24 -0500

    Importando el modulo de enrutamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PetspitModule } from './petspit/petspit.module';
 import { SharedModule } from './shared/shared.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
     AppRoutingModule,
     PetspitModule,
     SharedModule,
+    RouterModule,
     DashboardModule
   ],
   providers: [],
