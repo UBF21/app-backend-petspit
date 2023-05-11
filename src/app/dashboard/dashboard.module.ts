4c7dcc8e3999cf58e48377a844966fda8fb4dac1
UBF21 <flashgames104@gmail.com>
2023-05-11 16:11:52 -0500

    Importando los modulos necesarios para sar formularios en el modulo de Dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule, NgModel } from '@angular/forms';
+import { InterceptorRequestInterceptor } from '../interceptor/interceptor-request.interceptor';
 
 
 
@@ -10,14 +13,15 @@ import { SharedModule } from '../shared/shared.module';
     LoginPageComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     SharedModule
   ],
   exports: [
     LoginPageComponent
   ],
-  providers: [
-
-  ]
+  providers: []
 })
 export class DashboardModule { }
