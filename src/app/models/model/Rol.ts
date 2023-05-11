1e8d83ca77d151ed782497f9d0ba3f73e5228f11
UBF21 <flashgames104@gmail.com>
2023-05-11 16:16:33 -0500

    Creando la clase para Rol

---
+++ src/app/models/model/Rol.ts
@@ -0,0 +1,11 @@
+export class Rol {
+    idRol: number;
+    descripcion: string;
+    estado: string;
+
+    constructor() {
+        this.idRol = 0;
+        this.descripcion = "";
+        this.estado = "";
+    }
+}
