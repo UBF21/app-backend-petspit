f643a28f6579503f7b9d07d805ca61ca646ef251
UBF21 <flashgames104@gmail.com>
2023-05-10 20:22:48 -0500

    Creando la logica para enviar el correo de contacto

--- src/app/petspit/contact-page-petspit/contact-page-petspit.component.ts
+++ src/app/petspit/contact-page-petspit/contact-page-petspit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-page-petspit',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPagePetspitComponent implements OnInit {
 
-  constructor() { }
+  formEmail: FormGroup = new FormGroup({});
+
+
+  constructor(private formBulder: FormBuilder) { }
+
 
   ngOnInit(): void {
+    this.formEmail = this.formBulder.group({
+      nombre: ['', [Validators.required, Validators.maxLength(60)]],
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@gmail.com$/)]],
+      message: ['', [Validators.required, Validators.maxLength(255)]]
+    });
+
+  }
+
+  enviarEmail(): void {
+
+    emailjs.send('service_w8mxzgo', 'template_x0gkpkk', this.formEmail.value, 'hv85HqEeJ5R8JqIZY')
+      .then((response) => {
+        console.log('Correo enviado', response.status, response.text);
+      }).catch((error) => {
+        console.log("Error al enviar el correo.", error);
+      });
+
+      this.formEmail.reset();
   }
 
 }
