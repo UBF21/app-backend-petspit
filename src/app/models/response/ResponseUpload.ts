08f15da9dec37e5ab47a6018ddfdfb4f7bff6bbc
UBF21 <flashgames104@gmail.com>
2023-05-13 15:11:04 -0500

    Creando el modelo de respuesta de upload

---
+++ src/app/models/response/ResponseUpload.ts
@@ -0,0 +1,11 @@
+export class ResponseUpload{
+    imagen:string;
+    message:string;
+    date:string;
+
+    constructor(){
+        this.imagen = "";
+        this.message = "";
+        this.date = "";
+    }
+}
