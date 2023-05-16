a5fedf6925868886586438a67d9c5f974d1ec3a5
UBF21 <flashgames104@gmail.com>
2023-05-16 06:11:48 -0500

    llamamos al servicio de login y shared.Preguntamos si el usuario para mostrar no esta vacio el nombre para volver a cargar el usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from './services/auth/login.service';
+import { SharedInformationService } from './services/shared-information/shared-information.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'appbackendpetspit';
+  
+  constructor(private shared:SharedInformationService,private loginServices:LoginService){
+
+  }
+
+  ngOnInit(): void {
+    if(this.loginServices.getUserViewRecurrent().name !== ""){
+      this.shared.generateViewImageUser();
+    }
+  }
+
 }
