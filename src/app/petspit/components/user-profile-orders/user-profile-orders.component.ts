fa1d31faaa72e1d745e87fc64c0a00f63fca8081
UBF21 <flashgames104@gmail.com>
2023-05-16 15:42:05 -0500

    Creando la vista de las compras o ordenes realizadas por el cliente

---
+++ src/app/petspit/components/user-profile-orders/user-profile-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-orders',
+  templateUrl: './user-profile-orders.component.html',
+  styleUrls: ['./user-profile-orders.component.css']
+})
+export class UserProfileOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
