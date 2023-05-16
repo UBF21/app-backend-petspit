e1ddd6b617b242c1d5cee79d688082d63b8409bb
UBF21 <flashgames104@gmail.com>
2023-05-16 06:18:13 -0500

    llamamos al metodo para generar el usuario view para mostrarlo durante la sesion de la aplicación

--- src/app/dashboard/auth/login-page/login-page.component.ts
+++ src/app/dashboard/auth/login-page/login-page.component.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Observable, map } from 'rxjs';
 import { UserCredentials } from 'src/app/models/model/UserCredentials';
 import { ResponseUserCurrent } from 'src/app/models/response/ResponseUserRecurrent';
 import { LoginService } from 'src/app/services/auth/login.service';
+import { PublicUploadService } from 'src/app/services/public/upload/public-upload.service';
 import { SharedInformationService } from 'src/app/services/shared-information/shared-information.service';
 
 @Component({
@@ -13,31 +16,31 @@ import { SharedInformationService } from 'src/app/services/shared-information/sh
 })
 export class LoginPageComponent implements OnInit {
 
-  formLogin:FormGroup = new FormGroup({});
-  
-  constructor(private http:HttpClient,private loginService:LoginService,public shared : SharedInformationService,private formBuilder : FormBuilder) { }
-  
+  formLogin: FormGroup = new FormGroup({});
+
+  constructor(private http: HttpClient, private loginService: LoginService, public shared: SharedInformationService, private formBuilder: FormBuilder, private sanitizer: DomSanitizer, private publicUploadService: PublicUploadService) { }
+
   ngOnInit(): void {
 
     //Formulario de Inicio de Sesion
     this.formLogin = this.formBuilder.group({
-       username: ['',[Validators.required,Validators.maxLength(255),Validators.pattern(/^[a-zA-Z0-9@#$%&/()<>+*?¡¿!°|]+$/)]],
-       password:['',[Validators.required,Validators.maxLength(255),Validators.pattern(/^[a-zA-Z0-9@#$%&/()<>+*?¡¿!°|]+$/)]]   
+      username: ['', [Validators.required, Validators.maxLength(255), Validators.pattern(/^[a-zA-Z0-9@#$%&/()<>+*?¡¿!°|]+$/)]],
+      password: ['', [Validators.required, Validators.maxLength(255), Validators.pattern(/^[a-zA-Z0-9@#$%&/()<>+*?¡¿!°|]+$/)]]
     });
 
   }
 
   showRegistrationForm() {
-    this.shared.showRegistration = !this.shared.showRegistration; 
-    window.scrollTo(0,260);
+    this.shared.showRegistration = !this.shared.showRegistration;
+    window.scrollTo(0, 260);
   }
 
 
   //Seccion de Inicio de Sesion
-  getCurrentUser():void{
+  getCurrentUser(): void {
 
     this.loginService.getCurrentUser().subscribe({
-      next:(user:ResponseUserCurrent) => {
+      next: (user: ResponseUserCurrent) => {
         console.log(user);
         this.loginService.saveUserCurrent(user.user);
       },
@@ -48,29 +51,30 @@ export class LoginPageComponent implements OnInit {
 
   }
 
-  generateTokenUser():void{
-    
-    let credencials:UserCredentials = this.formLogin.value as UserCredentials;
-    this.loginService.generateToken(credencials)
-    .subscribe({
-      next:(token) =>{
-        console.log(token);
-        this.loginService.saveTokenUser(token.token);
-        
-        if(this.loginService.isLoggedIn()){
-          console.log("bienvenido usuario.");
-          this.getCurrentUser();
-        }else{
-          this.loginService.logout();
-        }
+  generateTokenUser(): void {
 
-      },
-      error: (error) => {console.log(error);}, 
-    });
+    let credencials: UserCredentials = this.formLogin.value as UserCredentials;
+    this.loginService.generateToken(credencials)
+      .subscribe({
+        next: (token) => {
+          console.log(token);
+          this.loginService.saveTokenUser(token.token);
+
+          if (this.loginService.isLoggedIn()) {
+            this.getCurrentUser();
+              
+          } else {
+            this.loginService.logout();
+          }
+
+        },
+        error: (error) => { console.log(error); },
+        complete:() => {setTimeout(()=> {this.shared.generateViewImageUser()},100) }
+      });
 
   }
 
-  signIn():void{
+  signIn(): void {
     console.log(this.formLogin.value);
     this.generateTokenUser();
     this.formLogin.reset();
