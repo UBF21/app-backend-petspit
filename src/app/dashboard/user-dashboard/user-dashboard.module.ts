d2481f255a8925739e41ac2217aa18a923ed7878
UBF21 <flashgames104@gmail.com>
2023-05-16 23:43:11 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/user-dashboard/user-dashboard.module.ts
+++ src/app/dashboard/user-dashboard/user-dashboard.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserPageListComponent } from './user-page-list/user-page-list.component';
+import { UserPageAddComponent } from './user-page-add/user-page-add.component';
+import { UserPageEditComponent } from './user-page-edit/user-page-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserPageListComponent,
+    UserPageAddComponent,
+    UserPageEditComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    UserPageListComponent,
+    UserPageAddComponent,
+    UserPageEditComponent
+  ],
+  providers:[
+    
   ]
 })
 export class UserDashboardModule { }
