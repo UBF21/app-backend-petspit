d96cbb763703228d52b2d8ac91394c869acc324a
UBF21 <flashgames104@gmail.com>
2023-05-16 20:58:08 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/marca-dashboard/marca-dashboard.module.ts
+++ src/app/dashboard/marca-dashboard/marca-dashboard.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MarcaPageListComponent } from './marca-page-list/marca-page-list.component';
+import { MarcaPageAddComponent } from './marca-page-add/marca-page-add.component';
+import { MarcaPageEditComponent } from './marca-page-edit/marca-page-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MarcaPageListComponent,
+    MarcaPageAddComponent,
+    MarcaPageEditComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    MarcaPageListComponent,
+    MarcaPageAddComponent,
+    MarcaPageEditComponent
+  ],
+  providers:[
+    
   ]
 })
 export class MarcaDashboardModule { }
