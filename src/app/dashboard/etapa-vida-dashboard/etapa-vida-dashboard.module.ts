b2d509d6b60cb9c1489f9e3754e7e086516d7f26
UBF21 <flashgames104@gmail.com>
2023-05-16 20:46:49 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/etapa-vida-dashboard/etapa-vida-dashboard.module.ts
+++ src/app/dashboard/etapa-vida-dashboard/etapa-vida-dashboard.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EtapaVidaPageListComponent } from './etapa-vida-page-list/etapa-vida-page-list.component';
+import { EtapaVidaPageAddComponent } from './etapa-vida-page-add/etapa-vida-page-add.component';
+import { EtapaVidaPageEditComponent } from './etapa-vida-page-edit/etapa-vida-page-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EtapaVidaPageListComponent,
+    EtapaVidaPageAddComponent,
+    EtapaVidaPageEditComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports:[],
+  exports:[
+    EtapaVidaPageListComponent,
+    EtapaVidaPageEditComponent,
+    EtapaVidaPageAddComponent
+  ],
   providers:[]
 })
 export class EtapaVidaDashboardModule { }
