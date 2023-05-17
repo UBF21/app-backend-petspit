d7c7f46f6ed07dfd96c6ee34cdd426d9acbdf1d3
UBF21 <flashgames104@gmail.com>
2023-05-16 23:44:11 -0500

    Creando el componente user-page-list, la vista para listar los usuarioss

---
+++ src/app/dashboard/user-dashboard/user-page-list/user-page-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserPageListComponent } from './user-page-list.component';
+
+describe('UserPageListComponent', () => {
+  let component: UserPageListComponent;
+  let fixture: ComponentFixture<UserPageListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserPageListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserPageListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/dashboard/user-dashboard/user-page-list/user-page-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-page-list',
+  templateUrl: './user-page-list.component.html',
+  styleUrls: ['./user-page-list.component.css']
+})
+export class UserPageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
