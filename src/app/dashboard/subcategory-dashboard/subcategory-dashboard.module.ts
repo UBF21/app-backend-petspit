25daaf4e536bcb863ee720b4d32da8cd8e8b6839
UBF21 <flashgames104@gmail.com>
2023-05-16 22:35:54 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/subcategory-dashboard/subcategory-dashboard.module.ts
+++ src/app/dashboard/subcategory-dashboard/subcategory-dashboard.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SubcategoryPageListComponent } from './subcategory-page-list/subcategory-page-list.component';
+import { SubcategoryPageAddComponent } from './subcategory-page-add/subcategory-page-add.component';
+import { SubcategoryPageEditComponent } from './subcategory-page-edit/subcategory-page-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SubcategoryPageListComponent,
+    SubcategoryPageAddComponent,
+    SubcategoryPageEditComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+
+  ],
+  providers:[
+    SubcategoryPageListComponent,
+    SubcategoryPageAddComponent,
+    SubcategoryPageEditComponent
   ]
 })
 export class SubcategoryDashboardModule { }
