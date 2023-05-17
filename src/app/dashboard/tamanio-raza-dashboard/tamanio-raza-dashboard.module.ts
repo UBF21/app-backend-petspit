fbf448d76c9aa297f2edafc31594ab3935fb86cc
UBF21 <flashgames104@gmail.com>
2023-05-16 23:27:15 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/tamanio-raza-dashboard/tamanio-raza-dashboard.module.ts
+++ src/app/dashboard/tamanio-raza-dashboard/tamanio-raza-dashboard.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TamanioRazaPageListComponent } from './tamanio-raza-page-list/tamanio-raza-page-list.component';
+import { TamanioRazaPageEditComponent } from './tamanio-raza-page-edit/tamanio-raza-page-edit.component';
+import { TamanioRazaPageAddComponent } from './tamanio-raza-page-add/tamanio-raza-page-add.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TamanioRazaPageListComponent,
+    TamanioRazaPageEditComponent,
+    TamanioRazaPageAddComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    TamanioRazaPageListComponent,
+    TamanioRazaPageEditComponent,
+    TamanioRazaPageAddComponent
+  ],
+  providers:[
+
   ]
 })
 export class TamanioRazaDashboardModule { }
