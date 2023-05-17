8294824d5ab8d1252954bb037fdf76b6f9169721
UBF21 <flashgames104@gmail.com>
2023-05-16 21:05:15 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/pedido-dashboard/pedido-dashboard.module.ts
+++ src/app/dashboard/pedido-dashboard/pedido-dashboard.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PedidoPageListComponent } from './pedido-page-list/pedido-page-list.component';
+import { PedidoPageAddComponent } from './pedido-page-add/pedido-page-add.component';
+import { PedidoPageEditComponent } from './pedido-page-edit/pedido-page-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PedidoPageListComponent,
+    PedidoPageAddComponent,
+    PedidoPageEditComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    PedidoPageListComponent,
+    PedidoPageAddComponent,
+    PedidoPageEditComponent
+  ],
+  providers:[
+
   ]
 })
 export class PedidoDashboardModule { }
