7447d660155d5806bb542dbb767fbd5b28811417
UBF21 <flashgames104@gmail.com>
2023-05-16 23:48:31 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/venta-dashboard/venta-dashboard.module.ts
+++ src/app/dashboard/venta-dashboard/venta-dashboard.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VentaPageListComponent } from './venta-page-list/venta-page-list.component';
+import { VentaPageAddComponent } from './venta-page-add/venta-page-add.component';
+import { VentaPageEditComponent } from './venta-page-edit/venta-page-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VentaPageListComponent,
+    VentaPageAddComponent,
+    VentaPageEditComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    VentaPageListComponent,
+    VentaPageAddComponent,
+    VentaPageEditComponent
+  ],
+  providers:[
+    
   ]
 })
 export class VentaDashboardModule { }
