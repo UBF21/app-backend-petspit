b639daad8d29a2088326751990be0f00d12f664e
UBF21 <flashgames104@gmail.com>
2023-05-17 02:16:48 -0500

    Importando el modulo RouterModule para mapear las rutas

--- src/app/dashboard/shared-dashboard/shared-dashboard.module.ts
+++ src/app/dashboard/shared-dashboard/shared-dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarDashboardComponent } from './sidebar-dashboard/sidebar-dashboard.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { SidebarDashboardComponent } from './sidebar-dashboard/sidebar-dashboard
     SidebarDashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     SidebarDashboardComponent
