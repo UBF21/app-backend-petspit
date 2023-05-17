6f2cda4b9001e95503849a84d5c7af97226427b8
UBF21 <flashgames104@gmail.com>
2023-05-17 01:58:58 -0500

    declarando y exportando los componentes creados (list,add y edit)

--- src/app/dashboard/category-dashboard/category-dashboard.module.ts
+++ src/app/dashboard/category-dashboard/category-dashboard.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoryPageListComponent } from './category-page-list/category-page-list.component';
+import { CategoryPageAddComponent } from './category-page-add/category-page-add.component';
+import { CategoryPageEditComponent } from './category-page-edit/category-page-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoryPageListComponent,
+    CategoryPageAddComponent,
+    CategoryPageEditComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    CategoryPageListComponent,
+    CategoryPageAddComponent,
+    CategoryPageEditComponent
+  ],
+  providers:[
+
   ]
 })
 export class CategoryDashboardModule { }
