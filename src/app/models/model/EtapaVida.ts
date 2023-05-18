4de914cd70d2503dc15b46bfcc9ba21f28e5047d
UBF21 <flashgames104@gmail.com>
2023-05-18 13:33:10 -0500

    Creando el modelo de Etapa de vida

---
+++ src/app/models/model/EtapaVida.ts
@@ -0,0 +1,14 @@
+export class EtapaVida {
+    idEtapaVida: number;
+    descripcion: string;
+    idAnimal: number;
+    estado: string;
+
+
+    constructor() {
+        this.idEtapaVida = 0,
+        this.descripcion = "";
+        this.idAnimal = 0;
+        this.estado = "";
+    }
+}
