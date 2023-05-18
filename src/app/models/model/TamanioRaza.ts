7d6ef832bd9ee311ae7e0c87f25aa30a840b7715
UBF21 <flashgames104@gmail.com>
2023-05-18 18:54:47 -0500

    Creando el modelo de tamaño de raza

---
+++ src/app/models/model/TamanioRaza.ts
@@ -0,0 +1,15 @@
+export class TamanioRaza{
+    idTamanioRaza:number;
+    descripcion:string;
+    idAnimal:number;
+    estado:string;
+
+
+    constructor()
+    {
+        this.idTamanioRaza =0;
+        this.descripcion = "";
+        this.idAnimal = 0;
+        this.estado = "";
+    }
+}
