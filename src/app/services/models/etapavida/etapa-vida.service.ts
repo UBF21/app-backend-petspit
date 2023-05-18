050759e07b2eef1d7e0bbc17f3ebf3dcd719552e
UBF21 <flashgames104@gmail.com>
2023-05-18 13:44:13 -0500

    Terminando el servicio de etapa de vida

--- src/app/services/models/etapavida/etapa-vida.service.ts
+++ src/app/services/models/etapavida/etapa-vida.service.ts
@@ -1,12 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EtapaVida } from 'src/app/models/model/EtapaVida';
+import { ResponseEtapaVida } from 'src/app/models/response/ResponseEtapaVida';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EtapaVidaService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  
+  public getAllEtapaVidas(): Observable<ResponseEtapaVida[]> {
+    return this.http.get<ResponseEtapaVida[]>(`${environment.backendUrl}api/etapaVida`);
+  }
+
+  public getEtapaVidaById(idEtapaVida: number): Observable<ResponseEtapaVida> {
+    return this.http.get<ResponseEtapaVida>(`${environment.backendUrl}api/etapaVida/${idEtapaVida}`);
+  }
+
+  public saveEtapaVida(etapaVida: EtapaVida): Observable<ResponseEtapaVida> {
+    return this.http.post<ResponseEtapaVida>(`${environment.backendUrl}api/etapaVida}`, etapaVida);
+  }
+
+  public deleteEtapaVida(idEtapaVida: number): Observable<ResponseEtapaVida> {
+    return this.http.delete<ResponseEtapaVida>(`${environment.backendUrl}api/etapaVida/${idEtapaVida}`);
+  }
 }
