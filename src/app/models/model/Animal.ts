c45df84203d46d4382643eeb5284cd254fd8d595
UBF21 <flashgames104@gmail.com>
2023-05-18 19:42:24 -0500

    Creando el modelo de animal

---
+++ src/app/models/model/Animal.ts
@@ -0,0 +1,11 @@
+export class Animal{
+    idAnimal:number;
+    claseAnimal:string;
+    estado:string;
+
+    constructor(){
+        this.idAnimal = 0;
+        this.claseAnimal = "";
+        this.estado = "";
+    }
+}
