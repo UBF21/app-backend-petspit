173bb8d02aed9658c911e9b6875d7e6f05532e4e
UBF21 <flashgames104@gmail.com>
2023-05-18 20:33:19 -0500

    Terminando el servicio product

--- src/app/services/models/product/product.service.ts
+++ src/app/services/models/product/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/model/Product';
+import { ResponseProduct } from 'src/app/models/response/ResponseProduct';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getAllProducts():Observable<Product[]>{
+    return this.http.get<Product[]>(`${environment.backendUrl}api/product`);
+  }
+
+  public getProductById(idProduct:number):Observable<ResponseProduct>{
+    return this.http.get<ResponseProduct>(`${environment.backendUrl}api/product/${idProduct}`);
+  }
+
+  public saveProduct(product:Product):Observable<ResponseProduct>{
+    return this.http.post<ResponseProduct>(`${environment.backendUrl}api/product`,product);
+  }
 }
