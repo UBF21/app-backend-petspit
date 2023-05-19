9e324136b3d6b0b3757704cac5c2e35e1d49d55c
UBF21 <flashgames104@gmail.com>
2023-05-18 20:38:08 -0500

    Creando el modelo de subCategory

---
+++ src/app/models/model/SubCategory.ts
@@ -0,0 +1,13 @@
+export class SubCategory {
+    idSubCategory: number;
+    descripcion: string;
+    estado: string;
+    idCategory: number;
+
+    constructor() {
+        this.idSubCategory = 0;
+        this.descripcion = "";
+        this.estado = "";
+        this.idCategory = 0;
+    }
+}
