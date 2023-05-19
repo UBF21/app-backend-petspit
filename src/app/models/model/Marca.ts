70c1fd73acd61aba012853e37351d333a7cf825e
UBF21 <flashgames104@gmail.com>
2023-05-19 00:49:15 -0500

    Agregando los atributos de dependencia a la clase Marca

--- src/app/models/model/Marca.ts
+++ src/app/models/model/Marca.ts
@@ -1,15 +1,18 @@
+import { Animal } from "./Animal";
+
 export class Marca {
     idMarca: number;
     descripcion: string;
     estado: string;
     imagen: string;
     idAnimal: number;
-
+    animal:Animal;
     constructor() {
         this.idMarca = 0;
         this.descripcion = "";
         this.estado = "";
         this.imagen = "";
         this.idAnimal = 0;
+        this.animal = new Animal();
     }
 }
