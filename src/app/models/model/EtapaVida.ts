70fc241dc46e5836a8329df57e95f752af8afa65
UBF21 <flashgames104@gmail.com>
2023-05-19 00:48:37 -0500

    Agregando los atributos de dependencia a la clase Etapa de  Vida

--- src/app/models/model/EtapaVida.ts
+++ src/app/models/model/EtapaVida.ts
@@ -1,14 +1,17 @@
+import { Animal } from "./Animal";
+
 export class EtapaVida {
     idEtapaVida: number;
     descripcion: string;
-    idAnimal: number;
     estado: string;
-
+    idAnimal: number;
+    animal:Animal;
 
     constructor() {
         this.idEtapaVida = 0,
         this.descripcion = "";
-        this.idAnimal = 0;
         this.estado = "";
+        this.idAnimal = 0;
+        this.animal = new Animal();
     }
 }
