c0615357d06f32f7c7547bf8a346d20f4e85fbd1
UBF21 <flashgames104@gmail.com>
2023-05-19 00:54:33 -0500

    Agregando los atributos de dependencia a la clase subCategory

--- src/app/models/model/SubCategory.ts
+++ src/app/models/model/SubCategory.ts
@@ -1,13 +1,17 @@
+import { Category } from "./Category";
+
 export class SubCategory {
     idSubCategory: number;
     descripcion: string;
     estado: string;
     idCategory: number;
+    category:Category;
 
     constructor() {
         this.idSubCategory = 0;
         this.descripcion = "";
         this.estado = "";
         this.idCategory = 0;
+        this.category = new Category();
     }
 }
