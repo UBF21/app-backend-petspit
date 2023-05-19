6b24dd3a95c1ed65dc412bdc4ecd14b80d84c2f8
UBF21 <flashgames104@gmail.com>
2023-05-18 19:49:02 -0500

    Terminando el servicio animal

--- src/app/services/models/animal/animal.service.ts
+++ src/app/services/models/animal/animal.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Animal } from 'src/app/models/model/Animal';
+import { ResponseAnimal } from 'src/app/models/response/ResponseAnimal';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getAllAnimals():Observable<Animal[]>{
+    return this.http.get<Animal[]>(`${environment.backendUrl}api/animal`);
+  }
+
+  public getAnimalById(idAnimal:number):Observable<ResponseAnimal>{
+    return this.http.get<ResponseAnimal>(`${environment.backendUrl}api/animal/${idAnimal}`);
+  }
+
+  public saveAnimal(animal:Animal):Observable<ResponseAnimal>{
+    return this.http.post<ResponseAnimal>(`${environment.backendUrl}api/animal`,animal);
+  } 
+
+  public deleteAnimal(idAnimal:number):Observable<ResponseAnimal>{
+    return this.http.delete<ResponseAnimal>(`${environment.backendUrl}api/animal/${idAnimal}`);
+  }
 }
