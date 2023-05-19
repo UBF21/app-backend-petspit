b2104531fb73ea8776292322f8f322558cb80f46
UBF21 <flashgames104@gmail.com>
2023-05-18 19:36:28 -0500

    Terminando el servicio de category

--- src/app/services/models/category/category.service.ts
+++ src/app/services/models/category/category.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/models/model/Category';
+import { ResponseCategory } from 'src/app/models/response/ResponseCategory';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.backendUrl}api/category`);
+  }
+
+  public getCategoryById(idCategory: number): Observable<ResponseCategory> {
+    return this.http.get<ResponseCategory>(`${environment.backendUrl}api/category/${idCategory}`);
+  }
+
+  public saveCategory(category: Category): Observable<ResponseCategory> {
+    return this.http.post<ResponseCategory>(`${environment.backendUrl}api/category`, category);
+  }
+
+  public deleteCategory(idCategory: number): Observable<ResponseCategory> {
+    return this.http.delete<ResponseCategory>(`${environment.backendUrl}api/category/${idCategory}`);
+  }
 }
