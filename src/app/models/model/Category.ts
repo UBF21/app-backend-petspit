571c29138e2f1c311f596bce3432e6322f70105e
UBF21 <flashgames104@gmail.com>
2023-05-19 00:47:20 -0500

    Agregando los atributos de dependencia a la clase Category

--- src/app/models/model/Category.ts
+++ src/app/models/model/Category.ts
@@ -1,13 +1,16 @@
+import { Animal } from "./Animal";
+
 export class Category {
     idCategory: number;
     descripcion: string;
     estado: string;
     idAnimal: number;
-
+    animal:Animal;
     constructor() {
         this.idCategory = 0;
         this.descripcion = "";
         this.estado = "";
         this.idAnimal = 0;
+        this.animal = new Animal();
     }
 }
