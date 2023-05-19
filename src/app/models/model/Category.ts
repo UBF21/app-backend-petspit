b6e6ef1813a3c0da695def3679b40f740600ed10
UBF21 <flashgames104@gmail.com>
2023-05-18 19:24:25 -0500

    Creando el modelo de category

---
+++ src/app/models/model/Category.ts
@@ -0,0 +1,13 @@
+export class Category {
+    idCategory: number;
+    descripcion: string;
+    estado: string;
+    idAnimal: number;
+
+    constructor() {
+        this.idCategory = 0;
+        this.descripcion = "";
+        this.estado = "";
+        this.idAnimal = 0;
+    }
+}
