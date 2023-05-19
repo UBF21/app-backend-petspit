cadf83e351b4f5bbf6664b49971275971c9b28b2
UBF21 <flashgames104@gmail.com>
2023-05-19 00:58:48 -0500

    Aggregando los importaciones necesariss

--- src/app/dashboard/etapa-vida-dashboard/etapa-vida-dashboard.module.ts
+++ src/app/dashboard/etapa-vida-dashboard/etapa-vida-dashboard.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { EtapaVidaPageListComponent } from './etapa-vida-page-list/etapa-vida-page-list.component';
 import { EtapaVidaPageAddComponent } from './etapa-vida-page-add/etapa-vida-page-add.component';
 import { EtapaVidaPageEditComponent } from './etapa-vida-page-edit/etapa-vida-page-edit.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
 
 
 
@@ -13,7 +16,11 @@ import { EtapaVidaPageEditComponent } from './etapa-vida-page-edit/etapa-vida-pa
     EtapaVidaPageEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule
   ],
   exports:[
     EtapaVidaPageListComponent,
