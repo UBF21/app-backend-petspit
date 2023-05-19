96e49d8ef51bfbcb723dafe4f1dd95439be7e0d7
UBF21 <flashgames104@gmail.com>
2023-05-18 21:07:05 -0500

    Terminando el servicio user

--- src/app/services/models/user/user.service.ts
+++ src/app/services/models/user/user.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/model/User';
+import { ResponseUser } from 'src/app/models/response/ResponseUser';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getAllUsers():Observable<User[]>{
+    return this.http.get<User[]>(`${environment.backendUrl}api/user`);
+  }
+
+  public getUserById(idUser:number):Observable<ResponseUser>{
+    return this.http.get<ResponseUser>(`${environment.backendUrl}api/user/${idUser}`);
+  }
+
+  public saveUser(user:User):Observable<ResponseUser>{
+    return this.http.post<ResponseUser>(`${environment.backendUrl}api/user`,user);
+  }
+
+  public deleteUser(idUser:number):Observable<ResponseUser>{
+    return this.http.delete<ResponseUser>(`${environment.backendUrl}api/user/${idUser}`);
+  }
 }
