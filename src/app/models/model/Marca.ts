bb0a5b918e20ca2868d6160c05883307ffb8d7dc
UBF21 <flashgames104@gmail.com>
2023-05-18 19:54:12 -0500

    Creando el modelo de marca

---
+++ src/app/models/model/Marca.ts
@@ -0,0 +1,15 @@
+export class Marca {
+    idMarca: number;
+    descripcion: string;
+    estado: string;
+    imagen: string;
+    idAnimal: number;
+
+    constructor() {
+        this.idMarca = 0;
+        this.descripcion = "";
+        this.estado = "";
+        this.imagen = "";
+        this.idAnimal = 0;
+    }
+}
