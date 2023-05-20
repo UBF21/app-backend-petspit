bb3ca5eb0afb5a5039cc62a3f30886c76f6cdc0b
UBF21 <flashgames104@gmail.com>
2023-05-20 16:29:18 -0500

    Mquetando el listado de category

--- src/app/dashboard/category-dashboard/category-page-list/category-page-list.component.ts
+++ src/app/dashboard/category-dashboard/category-page-list/category-page-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/model/Category';
+import { CategoryService } from 'src/app/services/models/category/category.service';
 
 @Component({
   selector: 'app-category-page-list',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryPageListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryServices: CategoryService) { }
 
   ngOnInit(): void {
+    this.getAllCategories();
   }
 
+  getAllCategories(): void {
+    this.categoryServices.getAllCategories()
+      .subscribe({
+        next: (response) => {
+          this.categories = response;
+          console.log(response);
+        },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  saveCategory(category: Category): void {
+    this.categoryServices.saveCategory(category)
+      .subscribe({
+        next: (response) => { console.log(response) },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  deleteCategory(idCategory: number): void {
+    let category: Category = this.categories.find(item => item.idCategory === idCategory)!;
+    if (category.estado === "I") {
+      console.log("El registro ya esta dehabilitado.");
+    } else {
+      category.estado = "I";
+      this.saveCategory(category);
+    }
+
+  }
 }
