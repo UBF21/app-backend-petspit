d6085e3b28b93d6f7287f203e9b6ba5d4adfd0ac
UBF21 <flashgames104@gmail.com>
2023-05-20 17:08:49 -0500

    Creando la logica para agregar una subCategory

--- src/app/dashboard/subcategory-dashboard/subcategory-page-add/subcategory-page-add.component.ts
+++ src/app/dashboard/subcategory-dashboard/subcategory-page-add/subcategory-page-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from 'src/app/models/model/Category';
+import { SubCategory } from 'src/app/models/model/SubCategory';
+import { CategoryService } from 'src/app/services/models/category/category.service';
+import { SubcategoryService } from 'src/app/services/models/subcategory/subcategory.service';
 
 @Component({
   selector: 'app-subcategory-page-add',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubcategoryPageAddComponent implements OnInit {
 
-  constructor() { }
+  formAddSubCategory: FormGroup = new FormGroup({});
+  categories: Category[] = [];
+
+  constructor(private formBuilder: FormBuilder, private categoryServices: CategoryService, private subCategoryServices: SubcategoryService) { }
 
   ngOnInit(): void {
+    this.getAllCategories();
+    this.formAddSubCategory = this.formBuilder.group({
+      descripcion: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
+      category: ['', [Validators.required]]
+    });
   }
 
+  getAllCategories() {
+    this.categoryServices.getAllCategories()
+      .subscribe({
+        next: (response) => {
+          this.categories = response;
+          console.log(response);
+        },
+        error: (error) => { console.log(error); }
+      });
+  }
+
+  fillSubCategoryForSave(): SubCategory {
+    let subCategory: SubCategory = new SubCategory();
+    subCategory.idSubCategory = 0;
+    subCategory.descripcion = this.formAddSubCategory.value.descripcion;
+    subCategory.estado = this.formAddSubCategory.value.estado;
+    subCategory.idCategory = this.formAddSubCategory.value.category;
+    subCategory.category = new Category();
+    subCategory.category.idCategory = this.formAddSubCategory.value.category;
+
+
+    return subCategory;
+  }
+
+  addSubCategory(subCategory: SubCategory): void {
+    this.subCategoryServices.saveSubCategory(subCategory)
+      .subscribe({
+        next: (response) => { console.log(response) },
+        error: (error) => { console.log(error); }
+      });
+  }
+
+  submitSubCategory(): void {
+    let subCategory: SubCategory = this.fillSubCategoryForSave();
+    this.addSubCategory(subCategory);
+    console.log(this.formAddSubCategory.value);
+    this.formAddSubCategory.reset();
+  }
 }
