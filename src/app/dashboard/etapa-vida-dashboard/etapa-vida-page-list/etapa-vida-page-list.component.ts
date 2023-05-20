2e79fb6060a6d1ed0b9b256d308839bab70d54cd
UBF21 <flashgames104@gmail.com>
2023-05-20 01:25:04 -0500

    Creando la logica para la vista de listado de etapadas de vida

--- src/app/dashboard/etapa-vida-dashboard/etapa-vida-page-list/etapa-vida-page-list.component.ts
+++ src/app/dashboard/etapa-vida-dashboard/etapa-vida-page-list/etapa-vida-page-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { error } from '@rxweb/reactive-form-validators';
+import { EtapaVida } from 'src/app/models/model/EtapaVida';
+import { EtapaVidaService } from 'src/app/services/models/etapavida/etapa-vida.service';
 
 @Component({
   selector: 'app-etapa-vida-page-list',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EtapaVidaPageListComponent implements OnInit {
 
-  constructor() { }
+  etapaVidas:EtapaVida[] = [];
+
+  constructor(private etapaVidaService:EtapaVidaService) { }
 
   ngOnInit(): void {
+    this.getAllEtapaVidas();
   }
 
+  getAllEtapaVidas(){
+    this.etapaVidaService.getAllEtapaVidas()
+    .subscribe({
+      next:(response)=>{
+        this.etapaVidas = response;
+        console.log(response);
+      },
+      error:(error)=>{console.log(error)}
+    });
+  }
+
+  saveEtapaVida(etapaVida:EtapaVida){
+      this.etapaVidaService.saveEtapaVida(etapaVida)
+      .subscribe({
+        next:(response)=>{console.log(response)},
+        error:(error)=>{console.log(error);}
+      });
+  }
+
+
+  deleteEtapaVida(idEtapaVida:number){
+    let etapaVida:EtapaVida = this.etapaVidas.find(value => value.idEtapaVida === idEtapaVida)!;
+    if(etapaVida.estado === "I") 
+    {
+      console.log("Ya esta deshabilitado.");
+    }else{
+      etapaVida.estado = "I";
+      this.saveEtapaVida(etapaVida);
+    }
+
+  }
 }
