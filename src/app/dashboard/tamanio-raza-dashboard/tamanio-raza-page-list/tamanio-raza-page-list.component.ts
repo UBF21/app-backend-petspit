8c88c3a5b27c43838c5a172e202fdbc8e35903f7
UBF21 <flashgames104@gmail.com>
2023-05-20 02:14:45 -0500

    Crando la logica para listar los tamaños de raza y el eliminar raza

--- src/app/dashboard/tamanio-raza-dashboard/tamanio-raza-page-list/tamanio-raza-page-list.component.ts
+++ src/app/dashboard/tamanio-raza-dashboard/tamanio-raza-page-list/tamanio-raza-page-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TamanioRaza } from 'src/app/models/model/TamanioRaza';
+import { TamanioRazaService } from 'src/app/services/models/tamanioraza/tamanio-raza.service';
 
 @Component({
   selector: 'app-tamanio-raza-page-list',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TamanioRazaPageListComponent implements OnInit {
 
-  constructor() { }
+  tamanioRazas: TamanioRaza[] = [];
+
+  constructor(private tamanioRazaServices: TamanioRazaService) { }
 
   ngOnInit(): void {
+    this.getAllTamanioRazas();
   }
 
+  getAllTamanioRazas() :void{
+
+    this.tamanioRazaServices.getAllTamanioRazas()
+      .subscribe({
+        next: (response) => {
+          this.tamanioRazas = response;
+          console.log(response);
+        },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  saveTamanioRaza(tamanioRaza:TamanioRaza):void{
+    this.tamanioRazaServices.saveTamanioRaza(tamanioRaza)
+    .subscribe({
+      next:(response)=>{console.log(response)},
+      error:(error)=> {console.log(error)}
+    });
+  }
+
+  deleteTamanioRaza(idTamanioRaza:number){
+
+    let tamanioRaza:TamanioRaza = this.tamanioRazas.find( item => item.idTamanioRaza === idTamanioRaza)!;
+    if(tamanioRaza.estado === 'I'){
+      console.log("Tamaño de raza deshabilitado.");
+    }
+    tamanioRaza.estado = "I";
+    this.saveTamanioRaza(tamanioRaza);
+  }
 }
