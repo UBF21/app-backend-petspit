4f88dda79daaae854b73500354f4ff7ea4e575f5
UBF21 <flashgames104@gmail.com>
2023-05-20 22:31:49 -0500

    Creando la logica para poder guardar una marca y la imagen de la marca

--- src/app/dashboard/marca-dashboard/marca-page-add/marca-page-add.component.ts
+++ src/app/dashboard/marca-dashboard/marca-page-add/marca-page-add.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RxwebValidators, file } from '@rxweb/reactive-form-validators';
+import { Animal } from 'src/app/models/model/Animal';
+import { Marca } from 'src/app/models/model/Marca';
+import { AnimalService } from 'src/app/services/models/animal/animal.service';
+import { MarcaService } from 'src/app/services/models/marca/marca.service';
+import { UploadImageService } from 'src/app/services/models/upload/upload-image.service';
 
 @Component({
   selector: 'app-marca-page-add',
@@ -7,9 +14,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarcaPageAddComponent implements OnInit {
 
-  constructor() { }
+  formAddMarca: FormGroup = new FormGroup({});
+  fileImage: File = new File([], "");
+  photoCargada: string | ArrayBuffer = "";
+  animales: Animal[] = [];
+
+
+  constructor(private formBuilder: FormBuilder, private animalService: AnimalService, private uploadImageService: UploadImageService, private marcaService: MarcaService) { }
 
   ngOnInit(): void {
+    this.getAllAnimals();
+    this.formAddMarca = this.formBuilder.group({
+      imagen: ['', [Validators.required, RxwebValidators.fileSize({ maxSize: 5 * 1024 * 1024, message: "El Máximo son 5MB." }), RxwebValidators.extension({ extensions: ['jpeg', 'jpg'], message: "La Imagen solo admite formato jpg." })]],
+      descripcion: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
+      animal: ['', [Validators.required]]
+    });
   }
 
+  getAllAnimals() {
+
+    this.animalService.getAllAnimals()
+      .subscribe({
+        next: (response) => { this.animales = response },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+
+  getImage(event: any) {
+
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.onload = (e) => this.photoCargada = reader.result!;
+      reader.readAsDataURL(event.target.files[0]);
+      this.fileImage = event.target.files[0];
+    } else {
+      this.photoCargada = "";
+    }
+  }
+
+  fillMarcaForSave(image:File){
+
+    let marca:Marca = new Marca();
+    marca.idMarca = 0;
+    marca.descripcion = this.formAddMarca.value.descripcion;
+    marca.idAnimal = this.formAddMarca.value.animal;
+    marca.estado = this.formAddMarca.value.estado;
+    marca.animal = new Animal();
+    marca.animal.idAnimal = this.formAddMarca.value.animal;
+    marca.imagen = image.name;
+
+    return marca;
+  }
+  
+  saveMarca(marca: Marca, image: File): void {
+
+    this.marcaService.saveMarca(marca)
+      .subscribe({
+        next: (response) => {
+          this.sendImageToMarca(image);
+          console.log(response);
+        },
+        error: () => { }
+      });
+  }
+
+  sendImageToMarca(image: File) {
+    const fileImage: FormData = new FormData();
+    fileImage.append("file", image, image.name);
+    this.uploadImageService.saveImageToMarca(fileImage)
+      .subscribe({
+        next: (response) => { console.log(response) },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+
+  submitMarca(): void {
+    let marca:Marca = this.fillMarcaForSave(this.fileImage);
+    this.saveMarca(marca,this.fileImage);
+    this.formAddMarca.reset();
+    this.formAddMarca.get('estado')?.setValue('');
+    this.formAddMarca.get('animal')?.setValue('');
+    this.photoCargada = "";
+  }
 }
