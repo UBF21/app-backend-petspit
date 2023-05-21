edbe9bcecece64d3373f57aefd22276ebb80dbf1
UBF21 <flashgames104@gmail.com>
2023-05-21 00:37:48 -0500

    Agregando las importaciones necesarias para el manejo de formularios y rutas en el modulo de user-dashboard

--- src/app/dashboard/user-dashboard/user-dashboard.module.ts
+++ src/app/dashboard/user-dashboard/user-dashboard.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { UserPageListComponent } from './user-page-list/user-page-list.component';
 import { UserPageAddComponent } from './user-page-add/user-page-add.component';
 import { UserPageEditComponent } from './user-page-edit/user-page-edit.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
 
 
 
@@ -13,7 +16,11 @@ import { UserPageEditComponent } from './user-page-edit/user-page-edit.component
     UserPageEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule
   ],
   exports:[
     UserPageListComponent,
