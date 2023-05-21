7b356393eb2fdc34b69197debcc3e059bac077de
UBF21 <flashgames104@gmail.com>
2023-05-21 00:37:32 -0500

    Agregando las importaciones necesarias para el manejo de formularios y rutas en el modulo de venta-dashboard

--- src/app/dashboard/venta-dashboard/venta-dashboard.module.ts
+++ src/app/dashboard/venta-dashboard/venta-dashboard.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { VentaPageListComponent } from './venta-page-list/venta-page-list.component';
 import { VentaPageAddComponent } from './venta-page-add/venta-page-add.component';
 import { VentaPageEditComponent } from './venta-page-edit/venta-page-edit.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
 
 
 
@@ -13,7 +16,11 @@ import { VentaPageEditComponent } from './venta-page-edit/venta-page-edit.compon
     VentaPageEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule
   ],
   exports:[
     VentaPageListComponent,
