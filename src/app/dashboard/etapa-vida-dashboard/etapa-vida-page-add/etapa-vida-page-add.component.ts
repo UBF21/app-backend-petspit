c30e1af17b9d8a953ccb366ac3e6f3fd15cc0738
UBF21 <flashgames104@gmail.com>
2023-05-21 03:24:15 -0500

    Seteando los select del formulario

--- src/app/dashboard/etapa-vida-dashboard/etapa-vida-page-add/etapa-vida-page-add.component.ts
+++ src/app/dashboard/etapa-vida-dashboard/etapa-vida-page-add/etapa-vida-page-add.component.ts
@@ -66,7 +66,7 @@ export class EtapaVidaPageAddComponent implements OnInit {
     this.addEtapaVida(etapaVida);
     console.log(this.formAddEtapaVida.value);
     this.formAddEtapaVida.reset();
-    this.formAddCategory.get('animal')?.setValue('');
-    this.formAddCategory.get('estado')?.setValue('');
+    this.formAddEtapaVida.get('animal')?.setValue('');
+    this.formAddEtapaVida.get('estado')?.setValue('');
   }
 }
