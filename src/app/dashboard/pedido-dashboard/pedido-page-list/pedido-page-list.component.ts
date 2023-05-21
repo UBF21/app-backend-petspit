2ef92f1b83f9a9ff6652b58030cc0ee33384b1b8
UBF21 <flashgames104@gmail.com>
2023-05-21 00:57:02 -0500

    Creando la logica para listar los pedidos y 'eliminar' un registro de pedido

--- src/app/dashboard/pedido-dashboard/pedido-page-list/pedido-page-list.component.ts
+++ src/app/dashboard/pedido-dashboard/pedido-page-list/pedido-page-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ValueProvider } from '@angular/core';
+import { Pedido } from 'src/app/models/model/Pedido';
+import { PedidoService } from 'src/app/services/models/pedido/pedido.service';
 
 @Component({
   selector: 'app-pedido-page-list',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoPageListComponent implements OnInit {
 
-  constructor() { }
+  pedidos: Pedido[] = [];
+
+  constructor(private pedidoServices: PedidoService) { }
 
   ngOnInit(): void {
   }
 
+
+  savePedido(pedido: Pedido): void {
+    this.pedidoServices.savePedido(pedido)
+      .subscribe({
+        next: (response) => { console.log(response) },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  getAllPedidos() {
+    this.pedidoServices.getAllPedidos()
+      .subscribe({
+        next: (response) => {
+          this.pedidos = response;
+          console.log(response);
+        },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  deletePedido(idPedido: number): void {
+
+    let pedido: Pedido = this.pedidos.find(item => item.idPedido === idPedido)!;
+    if (pedido.estado === "I") {
+      console.log("El Registro esta deshabilitado.");
+    } else {
+      pedido.estado = "I";
+      this.savePedido(pedido);
+    }
+  }
 }
