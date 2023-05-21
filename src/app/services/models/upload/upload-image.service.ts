3a4e75cabc4c85e1fa98e0396733893a4046dc0c
UBF21 <flashgames104@gmail.com>
2023-05-20 21:27:24 -0500

    Terminando el servico de uploadImage para las el manejo de archivos de imagen para marca,product y user

--- src/app/services/models/upload/upload-image.service.ts
+++ src/app/services/models/upload/upload-image.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResponseUpload } from 'src/app/models/response/ResponseUpload';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadImageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // upload para Marca
+  getImageToMarca(fileName:string): Observable<any> {
+    return this.http.get<any>(`${environment.backendUrl}upload/marca/image/${fileName}`);
+  }
+
+  saveImageToMarca(image:FormData): Observable<ResponseUpload> {
+    return this.http.post<ResponseUpload>(`${environment.backendUrl}upload/marca/image`, image);
+  }
+
+  deleteImageToMarca(fileName:string):Observable<ResponseUpload>{
+    return this.http.delete<ResponseUpload>(`${environment.backendUrl}upload/marca/image?filename=${fileName}`); 
+  }
+
+  //upload para Product
+  getImageToProduct(fileName:string): Observable<any> {
+    return this.http.get<any>(`${environment.backendUrl}upload/product/image/${fileName}`);
+  }
+
+  saveImageToProduct(image:FormData): Observable<ResponseUpload> {
+    return this.http.post<ResponseUpload>(`${environment.backendUrl}upload/product/image`, image);
+  }
+
+  deleteImageToProduct(fileName:string):Observable<ResponseUpload>{
+    return this.http.delete<ResponseUpload>(`${environment.backendUrl}upload/product/image?filename=${fileName}`);
+  }
+
+  //upload para User
+  public getImageToUser(fileName: string): Observable<any> {
+    return this.http.get<any>(`${environment.backendUrl}upload/user/image/${fileName}`, { responseType: "blob" as "json" });
+  }
+
+  public saveImageToUser(image: FormData): Observable<ResponseUpload> {
+    return this.http.post<ResponseUpload>(`${environment.backendUrl}upload/user/image`, image);
+  }
+
+  deleteImageToUser(fileName:string):Observable<ResponseUpload>{
+    return this.http.delete<ResponseUpload>(`${environment.backendUrl}upload/user/image?filename=${fileName}`);
+  }
+
 }
