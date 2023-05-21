d81b2b94d7fc1a4eebcf9c19d4d9e2b4a12e1c1a
UBF21 <flashgames104@gmail.com>
2023-05-21 01:36:06 -0500

    Listando los ventas en la vista de listado de ventas

--- src/app/dashboard/venta-dashboard/venta-page-list/venta-page-list.component.ts
+++ src/app/dashboard/venta-dashboard/venta-page-list/venta-page-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { find } from 'rxjs';
+import { Venta } from 'src/app/models/model/Venta';
+import { VentaService } from 'src/app/services/models/venta/venta.service';
 
 @Component({
   selector: 'app-venta-page-list',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VentaPageListComponent implements OnInit {
 
-  constructor() { }
+  ventas: Venta[] = [];
+
+  constructor(private ventaServices: VentaService) { }
 
   ngOnInit(): void {
   }
 
+  saveVenta(venta: Venta) {
+    this.ventaServices.saveVenta(venta)
+      .subscribe({
+        next: (response) => { console.log(response) },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  getAllVentas() {
+    this.ventaServices.getAllVentas()
+      .subscribe({
+        next: (response) => {
+          this.ventas = response;
+          console.log(response);
+        },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  deleteVenta(idVenta: number) {
+    let venta: Venta = this.ventas.find(item => item.idVenta === idVenta)!;
+    if (venta.estado === "I") {
+      console.log("El Registro esta deshabilitado.");
+    } else {
+      venta.estado = "I";
+      this.saveVenta(venta);
+    }
+  }
 }
