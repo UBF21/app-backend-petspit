0111ffa8982bbf7ede16ea3fe4b5b0b68f290129
UBF21 <flashgames104@gmail.com>
2023-05-25 16:47:13 -0500

    Creando la logica para mostrar la categoria y la subCategoria en el navs

--- src/app/shared/navs-petspit/navs-petspit.component.ts
+++ src/app/shared/navs-petspit/navs-petspit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Category } from 'src/app/models/model/Category';
+import { SubCategory } from 'src/app/models/model/SubCategory';
+import { PublicCategoryService } from 'src/app/services/public/category/public-category.service';
+import { PublicSubCategoryService } from 'src/app/services/public/subCategory/public-sub-category.service';
 
 @Component({
   selector: 'app-navs-petspit',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavsPetspitComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  Subcategories: SubCategory[] = [];
+
+  constructor(private publicCategoryServices: PublicCategoryService, private publicSubCategoryService: PublicSubCategoryService,private router:Router) { }
 
   ngOnInit(): void {
+    this.getAllCategories();
+    this.getAllSubCategories();
   }
 
+
+  getAllCategories(): void {
+    this.publicCategoryServices.getAllCategories()
+      .subscribe({
+        next: (response) => {
+          this.categories = response;
+          console.log(response);
+        },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+
+  getAllSubCategories(){
+
+    this.publicSubCategoryService.getAllSubCategories()
+    .subscribe({
+      next:(response)=>{
+          this.Subcategories = response;
+      },
+      error:(error)=>{console.log(error)}
+    });
+  }
+
+
+  routerOfCategory(descripcion:string){
+
+    this.router.navigate(['/products'],{queryParams :{category : descripcion}});
+  }
+
+  routerOfSubCategory(descripcion:string){
+    this.router.navigate(['/products'],{queryParams :{subCategory : descripcion}});
+  }
 }
