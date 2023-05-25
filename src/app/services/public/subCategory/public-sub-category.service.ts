c4ec153c1534a9e69d763c044be29688b18e346f
UBF21 <flashgames104@gmail.com>
2023-05-25 07:26:36 -0500

    Terminando servicio publico de subCategory

--- src/app/services/public/subCategory/public-sub-category.service.ts
+++ src/app/services/public/subCategory/public-sub-category.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SubCategory } from 'src/app/models/model/SubCategory';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicSubCategoryService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getAllSubCategories():Observable<SubCategory[]>{
+
+    return this.http.get<SubCategory[]>(`${environment.backendUrl}global/subCategory`);
+  }
+
+  public getAllSubCategoriesByIdCategory(idCategory:number):Observable<SubCategory[]>{
+    return this.http.get<SubCategory[]>(`${environment.backendUrl}global/subCategory/filter/${idCategory}`);
+  }
 }
