b8f21997d8c49222e94615f45ebbfbc05de59072
UBF21 <flashgames104@gmail.com>
2023-05-25 07:22:30 -0500

    Terminando el servicio public de category

--- src/app/services/public/category/public-category.service.ts
+++ src/app/services/public/category/public-category.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/models/model/Category';
+import { ResponseCategory } from 'src/app/models/response/ResponseCategory';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicCategoryService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  public getAllCategories():Observable<Category[]>{
+    return this.http.get<Category[]>(`${environment.backendUrl}global/category`);
+  }
+
+
+  public getCategoryById(idCategory:number):Observable<ResponseCategory>{
+   return this.http.get<ResponseCategory>(`${environment.backendUrl}global/category/${idCategory}`);
+  }
+
 }
