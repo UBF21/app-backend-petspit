2abe714d0882333a8bf1eedb31391bb4c86d29da
UBF21 <flashgames104@gmail.com>
2023-05-29 17:28:17 -0500

    Modificando la clase de subCategory para agregar un idAnimal y el animmal

--- src/app/models/model/SubCategory.ts
+++ src/app/models/model/SubCategory.ts
@@ -1,3 +1,4 @@
+import { Animal } from "./Animal";
 import { Category } from "./Category";
 
 export class SubCategory {
@@ -5,13 +6,17 @@ export class SubCategory {
     descripcion: string;
     estado: string;
     idCategory: number;
+    idAnimal:number;
     category:Category;
+    animal:Animal;
 
     constructor() {
         this.idSubCategory = 0;
         this.descripcion = "";
         this.estado = "";
         this.idCategory = 0;
+        this.idAnimal = 0;
         this.category = new Category();
+        this.animal = new Animal();
     }
 }
