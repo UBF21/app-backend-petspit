0c209c82724790aceb4498979906ba3722d69fab
UBF21 <flashgames104@gmail.com>
2023-05-30 00:40:26 -0500

    Creandola logica para listar los animales en el select, el evento al elegir un animal y se mostrará las categorias en base al animal

--- src/app/dashboard/subcategory-dashboard/subcategory-page-add/subcategory-page-add.component.ts
+++ src/app/dashboard/subcategory-dashboard/subcategory-page-add/subcategory-page-add.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map } from 'rxjs';
+import { Animal } from 'src/app/models/model/Animal';
 import { Category } from 'src/app/models/model/Category';
 import { SubCategory } from 'src/app/models/model/SubCategory';
+import { AnimalService } from 'src/app/services/models/animal/animal.service';
 import { CategoryService } from 'src/app/services/models/category/category.service';
 import { SubcategoryService } from 'src/app/services/models/subcategory/subcategory.service';
 
@@ -14,20 +17,27 @@ export class SubcategoryPageAddComponent implements OnInit {
 
   formAddSubCategory: FormGroup = new FormGroup({});
   categories: Category[] = [];
+  animals:Animal[] = [];
 
-  constructor(private formBuilder: FormBuilder, private categoryServices: CategoryService, private subCategoryServices: SubcategoryService) { }
+  idAnimalSelected: number = 0;
+
+  constructor(private formBuilder: FormBuilder, private categoryServices: CategoryService,private animalService:AnimalService, private subCategoryServices: SubcategoryService) { }
 
   ngOnInit(): void {
-    this.getAllCategories();
+    this.getAllAnimals();
     this.formAddSubCategory = this.formBuilder.group({
       descripcion: ['', [Validators.required, Validators.maxLength(255)]],
       estado: ['', [Validators.required]],
+      animal: ['', [Validators.required]],
       category: ['', [Validators.required]]
     });
+
+    this.formAddSubCategory.get('category')?.disable();
   }
 
-  getAllCategories() {
+  getAllCategories() :void{
     this.categoryServices.getAllCategories()
+      .pipe(map((response:Category[]) =>  response.filter(item => item.idAnimal === this.idAnimalSelected)))
       .subscribe({
         next: (response) => {
           this.categories = response;
@@ -37,6 +47,17 @@ export class SubcategoryPageAddComponent implements OnInit {
       });
   }
 
+  getAllAnimals():void{
+      this.animalService.getAllAnimals()
+      .subscribe(
+        {
+          next:(response) => this.animals = response,
+          error:(error) => console.log(error)
+        }
+      );
+  }
+
+
   fillSubCategoryForSave(): SubCategory {
     let subCategory: SubCategory = new SubCategory();
     subCategory.idSubCategory = 0;
@@ -45,7 +66,8 @@ export class SubcategoryPageAddComponent implements OnInit {
     subCategory.idCategory = this.formAddSubCategory.value.category;
     subCategory.category = new Category();
     subCategory.category.idCategory = this.formAddSubCategory.value.category;
-
+    subCategory.idAnimal = this.formAddSubCategory.value.animal;
+    subCategory.animal.idAnimal = this.formAddSubCategory.value.animal;
 
     return subCategory;
   }
@@ -58,12 +80,26 @@ export class SubcategoryPageAddComponent implements OnInit {
       });
   }
 
+  //evento
+  onChangeAnimal(event:any){
+
+    if (event.target.value === '') {
+      this.formAddSubCategory.get('category')?.disable();
+    }else{
+      this.idAnimalSelected = +event.target.value;
+      this.getAllCategories();
+      this.formAddSubCategory.get('category')?.enable();
+    }
+  } 
+
+
   submitSubCategory(): void {
     let subCategory: SubCategory = this.fillSubCategoryForSave();
     this.addSubCategory(subCategory);
     console.log(this.formAddSubCategory.value);
     this.formAddSubCategory.reset();
     this.formAddSubCategory.get('animal')?.setValue('');
+    this.formAddSubCategory.get('estado')?.setValue('');
     this.formAddSubCategory.get('category')?.setValue('');
   }
 }
