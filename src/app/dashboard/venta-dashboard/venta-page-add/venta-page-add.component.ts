e0afaca9a93117288e33c803c2861f62c7f7e424
UBF21 <flashgames104@gmail.com>
2023-06-12 15:50:38 -0500

    Quitando la logica de prueba

--- src/app/dashboard/venta-dashboard/venta-page-add/venta-page-add.component.ts
+++ src/app/dashboard/venta-dashboard/venta-page-add/venta-page-add.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterContentInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+// @ts-ignore
+import SwaggerUI from 'swagger-ui';
 @Component({
   selector: 'app-venta-page-add',
   templateUrl: './venta-page-add.component.html',
   styleUrls: ['./venta-page-add.component.css']
 })
 export class VentaPageAddComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+    const ui = SwaggerUI({
+        url: 'http://localhost:8080/v3/api-docs',
+        dom_id: '#customer-api-documentation'
+    })
+
   }
 
 }
