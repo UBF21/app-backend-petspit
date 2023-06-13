86f01382f40c841820ca4b0110a072b55cd7aa86
UBF21 <flashgames104@gmail.com>
2023-06-13 05:09:38 -0500

    Agregand ola logica para incorporar el editor de texto

--- src/app/petspit/contact-page-petspit/contact-page-petspit.component.ts
+++ src/app/petspit/contact-page-petspit/contact-page-petspit.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import emailjs from 'emailjs-com';
+import { TinyMCE,Editor } from 'tinymce';
+declare const tinymce: TinyMCE;
 
 @Component({
   selector: 'app-contact-page-petspit',
   templateUrl: './contact-page-petspit.component.html',
   styleUrls: ['./contact-page-petspit.component.css']
 })
-export class ContactPagePetspitComponent implements OnInit {
+export class ContactPagePetspitComponent implements OnInit,AfterViewInit {
 
   formEmail: FormGroup = new FormGroup({});
-
+  editorConfig:any = {
+    height: 900,
+    plugins:'lists link image table code help wordcount' ,
+    toolbar: 'undo redo | formatselect | bold italic underline | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist',
+    menubar: false,
+    toolbar_drawer: 'floating'
+  };
 
   constructor(private formBulder: FormBuilder) { }
 
@@ -19,12 +27,20 @@ export class ContactPagePetspitComponent implements OnInit {
     this.formEmail = this.formBulder.group({
       nombre: ['', [Validators.required, Validators.maxLength(60)]],
       email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@gmail.com$/)]],
-      message: ['', [Validators.required, Validators.maxLength(255)]]
+      message: ['', [Validators.required, Validators.maxLength(20000)]]
     });
 
   }
 
+  ngAfterViewInit(): void {
+    tinymce.init({
+      selector:"#messageEditor",
+      ...this.editorConfig
+    })
+  }
+
   enviarEmail(): void {
+
 
     emailjs.send('service_w8mxzgo', 'template_x0gkpkk', this.formEmail.value, 'hv85HqEeJ5R8JqIZY')
       .then((response) => {
