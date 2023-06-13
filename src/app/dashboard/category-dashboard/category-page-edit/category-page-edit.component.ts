3ba072bb1ca2655d0d74afd346e94fcba81a2d17
UBF21 <flashgames104@gmail.com>
2023-06-13 06:09:01 -0500

    Creando la logica previa para el editar category

--- src/app/dashboard/category-dashboard/category-page-edit/category-page-edit.component.ts
+++ src/app/dashboard/category-dashboard/category-page-edit/category-page-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Animal } from 'src/app/models/model/Animal';
+import { Category } from 'src/app/models/model/Category';
+import { AnimalService } from 'src/app/services/models/animal/animal.service';
+import { CategoryService } from 'src/app/services/models/category/category.service';
 
 @Component({
   selector: 'app-category-page-edit',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryPageEditComponent implements OnInit {
 
-  constructor() { }
+  formAddCategory: FormGroup = new FormGroup({});
+  animales: Animal[] = [];
+
+
+  constructor(private formBuilder: FormBuilder, private animalService: AnimalService, private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.getAllAnimals();
+    this.formAddCategory = this.formBuilder.group({
+      descripcion: ['', [Validators.required,Validators.maxLength(255)]],
+      estado: ['', [Validators.required]],
+      animal: ['', [Validators.required]],
+    });
   }
 
+  getAllAnimals() {
+
+    this.animalService.getAllAnimals()
+      .subscribe({
+        next: (response) => { this.animales = response },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  addCategory(category: Category): void {
+
+    this.categoryService.saveCategory(category)
+      .subscribe({
+        next: (response) => { console.log(response) },
+        error: (error) => { console.log(error) }
+      });
+  }
+
+  fillCategoryOfSave(): Category {
+    let category: Category = new Category();
+    category.idCategory = 0;
+    category.descripcion = this.formAddCategory.value.descripcion;
+    category.idAnimal = this.formAddCategory.value.animal;
+    category.estado = this.formAddCategory.value.estado;
+    category.animal = new Animal();
+    category.animal.idAnimal = this.formAddCategory.value.animal;
+
+    return category;
+  }
+
+  submitCategory():void{
+    let category:Category = this.fillCategoryOfSave();
+    this.addCategory(category);
+    console.log(this.formAddCategory.value);
+    this.formAddCategory.reset();
+    this.formAddCategory.get('animal')?.setValue('');
+    this.formAddCategory.get('estado')?.setValue('');
+  }
 }
