55d79d7e95bee61ead4dcc5a5c48988890250047
UBF21 <flashgames104@gmail.com>
2023-06-20 10:22:51 -0500

    Agregando el metodo para poder actualziar el listado y obtener un regitro del carrito para cambiar algo

--- src/app/services/carrito/carrito.service.ts
+++ src/app/services/carrito/carrito.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Pedido } from 'src/app/models/model/Pedido';
+import { Product } from 'src/app/models/model/Product';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +22,36 @@ export class CarritoService {
       return pedidos; 
     }else return [];
     
+  }
+
+  public isItemExist(producto:Product):boolean{
+    let items = localStorage.getItem("items");
+    let pedidos:Pedido[] = [];
+    if(items !== null){
+      pedidos = JSON.parse(items);
+      return pedidos.some(item => item.product.idProduct === producto.idProduct);
+    }else{
+      pedidos = [];
+      return false;
+    }
+  }
+
+  public getIndexItemCarrito(product:Product){
+    let items = localStorage.getItem("items");
+    let pedidos:Pedido[] = [];
+    let index  = 0;
+    
+    if(items !== null){
+      pedidos = JSON.parse(items);
+
+      index = pedidos.findIndex(item => item.idProduct === product.idProduct);
+    }
+  
+    return index;
+  }
+
+  updateListCarrito(pedidos:Pedido[]){
+    localStorage.setItem("items",JSON.stringify(pedidos));
   }
 
   public additemCarrito(pedido:Pedido):void{
