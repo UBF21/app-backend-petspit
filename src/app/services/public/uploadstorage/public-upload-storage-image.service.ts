ced277ed61fc1c85c13ef6a734f45aff0fe0503d
UBF21 <flashgames104@gmail.com>
2023-06-27 16:41:29 -0500

    Agregando el metodo de guardar imagen de user en el servidor compartido de archivos azure

--- src/app/services/public/uploadstorage/public-upload-storage-image.service.ts
+++ src/app/services/public/uploadstorage/public-upload-storage-image.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ResponseUpload } from 'src/app/models/response/ResponseUpload';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -16,6 +17,11 @@ export class PublicUploadStorageImageService {
 
   public getPublicImageMarca(filename: string): Observable<any> {
     return this.http.get<any>(`${environment.backendUrl}global/upload-storage/marca/image/${filename}`, { responseType: "blob" as "json" });
+  }
+
+
+  public saveImageUser(image: FormData): Observable<ResponseUpload> {
+    return this.http.post<ResponseUpload>(`${environment.backendUrl}global/upload-storage/user/image`, image);
   }
 
   public getPublicImageUser(filename: string): Observable<any> {
