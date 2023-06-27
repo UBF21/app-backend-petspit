054b4d2c127b52cc440e4e9d41950ffee3a82432
UBF21 <flashgames104@gmail.com>
2023-06-27 18:28:17 -0500

    Creando la lógica del home

--- src/app/petspit/home-page-petspit/home-page-petspit.component.ts
+++ src/app/petspit/home-page-petspit/home-page-petspit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { ViewImageMarca } from 'src/app/models/interfaces/ViewImageMarca';
+import { Marca } from 'src/app/models/model/Marca';
+import { PublicMarcaService } from 'src/app/services/public/marca/public-marca.service';
+import { PublicUploadStorageImageService } from 'src/app/services/public/uploadstorage/public-upload-storage-image.service';
 
 @Component({
   selector: 'app-home-page-petspit',
@@ -7,10 +14,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePagePetspitComponent implements OnInit {
 
-  constructor() { }
+  marcas: Marca[] = [];
+  viewMarcas: ViewImageMarca[] = [];
+
+  loadingMarca: boolean = true;
+
+  constructor(private publicMarcaService: PublicMarcaService, private publicUploadStorageImageService: PublicUploadStorageImageService, private sanitizer: DomSanitizer,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getAllMarcas();
+  }
 
-}
+  getImageMarca(fileName: string): Observable<SafeUrl> {
+    return this.publicUploadStorageImageService.getPublicImageMarca(fileName)
+      .pipe(map((response) => {
+        URL.revokeObjectURL(response);
+        const url: string = URL.createObjectURL(response);
+        return this.sanitizer.bypassSecurityTrustUrl(url);
+      }));
+  }
+
+  getAllMarcas(): void {
+    this.publicMarcaService.getAllMarcas()
+      .pipe()
+      .subscribe({
+        next: (response) => {
+          this.viewMarcas = [];
+          this.marcas = response;
+          this.fillViewProducts();
+          setTimeout(() => { this.loadingMarca = false }, 2000);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+  }
+
+  fillViewProducts(): void {
+
+    this.marcas.forEach((item) => {
+      this.getImageMarca(item.imagen)
+        .subscribe({
+          next: (response) => {
+            this.addRowViewMarca(response, item);
+          },
+          error: (error) => { console.log(error) }
+        });
+    });
+
+    setTimeout(() => { }, 2000)
+  }
+
+  routerOfAllProducts() {
+    this.router.navigate(['/products'], { queryParams: { todas: "all" } });
+  }
+
+  addRowViewMarca(image: SafeUrl, marca: Marca): void {
+    this.viewMarcas.push({ pathName: image, name: marca.imagen, marca: marca });
+  }
 
 }
